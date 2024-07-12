fbe0bd68575cea58634bf92eef19231a6beed967
Stalin1824 <160157201+Stalin1824@users.noreply.github.com>
2024-07-12 19:41:50 +0530

    Add files via upload

---
+++ Day5/main.js
@@ -0,0 +1,36 @@
+
+// toggel functionality
+
+let a =true;
+function UI(){
+
+  UI.prototype.toggle= function(){
+ const shadow = document.querySelector(".shadow") 
+    if(a){
+      shadow.style.transform ="scale(0)"
+      shadow.style.transition ="1s"
+       
+    }else{
+        shadow.style.transform ="scale(1)"
+        shadow.style.transition ="1s"
+    
+    }
+
+}
+
+}
+
+
+
+
+
+document.querySelector(".fa-bell"),addEventListener(("click"),()=>{
+   const  ui =new UI()
+      ui.toggle()
+      a=!a
+
+})
+
+
+
+
